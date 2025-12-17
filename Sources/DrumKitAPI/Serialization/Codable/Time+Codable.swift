1b4f61647525949e0a4820e0622b32edcc02145d
Jordan Kay <jordan@fleuronic.com>
2025-12-17 10:35:28 -0500

    Fix warnings

--- Sources/DrumKitAPI/Serialization/Codable/Time+Codable.swift
+++ Sources/DrumKitAPI/Serialization/Codable/Time+Codable.swift
@@ -1,14 +1,17 @@
 import struct DrumKit.Time
 
-extension Time: Codable {
+extension Time: Swift.Decodable {
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.singleValueContainer()
+		self = try Time.value.decode(container.decode(String.self)).get()
+	}
+}
+
+// MARK: -
+extension Time: Swift.Encodable {
 	public func encode(to encoder: Encoder) throws {
 		var container = encoder.singleValueContainer()
 		try container.encode(Time.value.encode(self))
 
-	}
-
-	public init(from decoder: Decoder) throws {
-		let container = try decoder.singleValueContainer()
-		self = try Time.value.decode(container.decode(String.self)).get()
 	}
 }
